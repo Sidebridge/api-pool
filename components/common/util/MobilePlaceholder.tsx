afe9717d0d251be0a5fdba8c30b71bbe5d08bdc4
GeekyAdams <drkeeminc@gmail.com>
2023-05-17 20:48:31 +0100

    feat: mobile coming soon placeholder UI adjustments

--- components/common/util/MobilePlaceholder.tsx
+++ components/common/util/MobilePlaceholder.tsx
@@ -9,17 +9,17 @@ const MobilePlaceholder = () => {
     <div className="align-col text-center items-center md:hidden py-8 px-5 fixed top-0 left-0 w-screen h-screen bg-body z-[3000]">
       <AppLogo />
 
-      <AppIcon icon="MobileSoon" styles="mt-10 min-w-[300px]" />
+      <AppIcon icon="MobileSoon" styles="mt-5 min-w-[300px]" />
 
-      <h1 className={clsx("text-3xl mt-16", classes["header-text__bg"])}>
+      <h1 className={clsx("text-3xl mt-12", classes["header-text__bg"])}>
         Mobile Availability Not Ready Yet!
       </h1>
-      <p className="mt-8 text-lg font-light text-grey-legacy">
+      <p className="mt-6 text-lg font-light text-grey-legacy">
         We are not convinced enough yet on why we should spend some time to work
         on this app&apos;s mobile responsiveness 😬, but i assure you that the
         team is not relenting. <br />
         In the meantime, you can continue to explore APIs and have fun doing
-        that on devices with bigger screens like a tablet or PC 💻
+        that on devices with larger screens like a tablet or PC 💻
       </p>
     </div>
   );
